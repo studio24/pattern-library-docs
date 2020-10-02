fbb4902467e84392cddf9971b75abd9c475a8c8a
Nicola Saunders <nicola@studio24.net>
2020-10-02 12:52:58 +0100

    Update dev dependencies and build script

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 // Get our requirements, installed by npm
 const Metalsmith = require('metalsmith'); // Static site generator
+const inPlace = require('metalsmith-in-place'); // Render templating syntax in your source files
 
 
 // Run Metalsmith in the current directory
@@ -8,7 +9,7 @@ Metalsmith(__dirname)
 	.source('src') // Set page source directory
 	.destination('build') // Set destination directory
 
-	// And tell Metalsmith to build the site
+	// Tell Metalsmith to build the site
 	.build(function(err, files) {
 		if (err) { throw err; }
 	});
