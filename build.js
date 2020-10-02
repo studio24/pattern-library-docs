c373b32a97ad84a9b8cada30e140203041d2762b
Nicola Saunders <nicola@studio24.net>
2020-10-02 12:06:59 +0100

    Update build.js

--- build.js
+++ build.js
@@ -1,12 +1,16 @@
 // Get our requirements, installed by npm
-const Metalsmith = require('metalsmith'); // Static site generator.
-const markdown = require('metalsmith-markdown');
+const Metalsmith = require('metalsmith'); // Static site generator
+const markdown = require('metalsmith-markdown'); // Markdown converter
 
-// Run Metalsmith in the current directory.
+// Run Metalsmith in the current directory
 Metalsmith(__dirname)
-	.destination('./build')
+	.clean(true) // Clean the build directory
+	.source('src') // Set page source directory
+	.destination('build') // Set destination directory
 
-	// And tell Metalsmith to fire it all off.
+	.use(markdown()) // Convert markdown to HTML
+
+	// And tell Metalsmith to build the site
 	.build(function(err, files) {
 		if (err) { throw err; }
 	});
