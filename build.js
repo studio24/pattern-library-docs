858723cdcf4063f860c652c72f8ab30abf011e03
Nicola Saunders <nicola@studio24.net>
2020-10-02 12:23:28 +0100

    remove metalsmith-markdown dev dependency, update build script

--- build.js
+++ build.js
@@ -1,14 +1,12 @@
 // Get our requirements, installed by npm
 const Metalsmith = require('metalsmith'); // Static site generator
-const markdown = require('metalsmith-markdown'); // Markdown converter
+
 
 // Run Metalsmith in the current directory
 Metalsmith(__dirname)
 	.clean(true) // Clean the build directory
 	.source('src') // Set page source directory
 	.destination('build') // Set destination directory
-
-	.use(markdown()) // Convert markdown to HTML
 
 	// And tell Metalsmith to build the site
 	.build(function(err, files) {
