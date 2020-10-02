da09b6606d5f254925d57b4dc76db335ed3328f1
Nicola Saunders <nicola@studio24.net>
2020-10-02 12:00:04 +0100

    Add metalsmith-markdown dev dependency, update build script

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 // Get our requirements, installed by npm
 const Metalsmith = require('metalsmith'); // Static site generator.
+const markdown = require('metalsmith-markdown');
 
 // Run Metalsmith in the current directory.
 Metalsmith(__dirname)
